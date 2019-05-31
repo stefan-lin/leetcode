4ca19e8ad24ae4d876cdb3103ac262871fcf04f4
Stefan <yien.lin@sjsu.edu>
2019-05-30 20:06:47 -0700

    addtwonums not done

---
+++ addtwonums/py/addtwonums.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+from typing import List
+
+"""
+You are given two non-empty linked lists representing two non-negative 
+integers. The digits are stored in reverse order and each of their nodes 
+contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the 
+number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+
+"""
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode)-> ListNode:
+        carry_ = 0
+        run1 = l1
+        run2 = l2
+        ret_node = None
+        run_ret = None
+
+        while run1 and run2:
+            sum_node = run1.val + run2.val
+
+            if not ret_node:
+                ret_node = ListNode(sum_node % 10 + carry_)
+                run_ret = ret_node
+            else:
+                ret_node.next = ListNode(sum_node % 10 + carry_)
+                ret_node = ret_node.next
+            
+            carry_ = sum_node // 10
+            run1 = run1.next
+            run2 = run2.next
+        else:
+            while run1:
+                if not ret_node:
+                    ret_node = ListNode((run1.val + carry_) % 10)
+                    run_ret  = ret_node
+
+                else:
+                    run_ret.next = ListNode((run1.val + carry_) % 10)
+                    run_ret = run_ret.next
+                carry_ = (run1.val + carry_) // 10
+            while run2:
+                if not ret_node:
+                    ret_node = ListNode((run2.val + carry_) % 10)
+                    run_ret = ret_node
+                else:
+                    run_ret.next = ListNode((run2.val + carry_) % 10)
+                    run_ret = ret_node.next
+                carry_ = (run2.val + carry_) // 10
+        return ret_node
+    # END addTwoNumbers 
+
+    def print_solution(self, input: ListNode):
+        r_list = []
+        runner = input
+
+        while runner:
+            r_list.insert(0, runner.val)
+            runner = runner.next
+
+        print(''.join(map(str, r_list)))
+
+    def build_listnode(self, input: List[int])-> ListNode:
+        m = None
+        rm = None
+        for i in input:
+            if not m:
+                m = ListNode(i)
+                rm = m
+            else:
+                rm.next = ListNode(i)
+                rm = rm.next
+        return m
